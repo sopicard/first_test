369eda08271a32c051530b7035b5cf88a219623c
sopicard <sogautrelet@gmail.com>
2022-07-01 13:55:18 +0200

    ajout d'une route personalities avec cible d'id

--- src/Controller/StarsController.php
+++ src/Controller/StarsController.php
@@ -31,7 +31,7 @@ class StarsController extends AbstractController
             3 => [
                 'title' => 'Afida Turner',
                 'color' => 'black',
-                'published' => true,
+                'published' => false,
                 'image' => 'https://s.yimg.com/uu/api/res/1.2/SXrg2uonJIrD9JlemslJWQ--~B/aD01Njg7dz0xMDI0O2FwcGlkPXl0YWNoeW9u/http://media.zenfs.com/fr_FR/News/TeleLoisirs/96851-afida-turner-atteinte-de-deux-tumeurs-elle-sera-bientot-hospitalisee.jpeg',
                 'publishedDate' => new \DateTime('NOW')
             ],
@@ -45,13 +45,62 @@ class StarsController extends AbstractController
             5 => [
                 'title' => 'JCVD',
                 'color' => 'blue',
-                'published' => false,
+                'published' => true,
                 'image' => 'https://www.programme.tv/imgre/fit/~1~tel~2021~07~12~6801abcb-554d-4a8b-8667-d05cb7f88734.png/1160x500/crop-from/top/quality/80/focus-point/1783,871/jean-claude-van-damme-devoile-les-causes-de-ses-problemes-de-drogue.jpg',
                 'publishedDate' => new \DateTime('NOW')
             ]
         ];
 
-        return $this-> render ("stars.html.twig",["stars"=>$stars]);
+        return $this->render("stars.html.twig", ["stars" => $stars]);
+    }
+    // je crée une nouvelle route ainsi su'une nouvelle fonction nommées différemment mais avec le même "fake" tableau
+    // que Stars pour cibler grâce à l'id
+    /**
+     * @Route ("/personalities/{id}", name="personalities")
+     */
+
+    public function personalities($id)
+    {
+        $personalities = [
+            1 => [
+                'title' => 'Tex',
+                'color' => 'red',
+                'published' => false,
+                'image' => 'https://resize.programme-television.ladmedia.fr/r/670,670/img/var/premiere/storage/images/tele-7-jours/news-tv/video-les-z-amours-france-2-tex-craque-sa-chemise-devant-les-candidats-4344637/77880010-1-fre-FR/VIDEO-Les-Z-amours-France-2-Tex-craque-sa-chemise-devant-les-candidats.jpg',
+                'publishedDate' => new \DateTime('NOW')
+            ],
+            2 => [
+                'title' => 'Cyril Hanouna',
+                'color' => 'yellow',
+                'published' => true,
+                'image' => 'https://media.onvoitout.fr/2021/10/cyril-hanouna-tpmp-stresse-tres-inquiet-pour-les-yeux-de-benjamin-castaldi-il-dit-tout-50.jpg',
+                'publishedDate' => new \DateTime('NOW')
+            ],
+            3 => [
+                'title' => 'Afida Turner',
+                'color' => 'black',
+                'published' => false,
+                'image' => 'https://s.yimg.com/uu/api/res/1.2/SXrg2uonJIrD9JlemslJWQ--~B/aD01Njg7dz0xMDI0O2FwcGlkPXl0YWNoeW9u/http://media.zenfs.com/fr_FR/News/TeleLoisirs/96851-afida-turner-atteinte-de-deux-tumeurs-elle-sera-bientot-hospitalisee.jpeg',
+                'publishedDate' => new \DateTime('NOW')
+            ],
+            4 => [
+                'title' => 'Manuel Valls',
+                'color' => 'blue',
+                'published' => false,
+                'image' => 'https://images.bfmtv.com/vypLwZsN4IyBgQiA4BpWRJgRrpg=/4x3:1252x705/1248x0/images/-80516.jpg',
+                'publishedDate' => new \DateTime('NOW')
+            ],
+            5 => [
+                'title' => 'JCVD',
+                'color' => 'blue',
+                'published' => true,
+                'image' => 'https://www.programme.tv/imgre/fit/~1~tel~2021~07~12~6801abcb-554d-4a8b-8667-d05cb7f88734.png/1160x500/crop-from/top/quality/80/focus-point/1783,871/jean-claude-van-damme-devoile-les-causes-de-ses-problemes-de-drogue.jpg',
+                'publishedDate' => new \DateTime('NOW')
+            ]
+        ];
+
+        return $this->render("personalities.html.twig", ["personalities" => $personalities[$id]]);
+
     }
 }
-// sur ce coup aucune envie d'écrire un commentaire après avoir passé 20 min à mettre une bordure horrible !!!!
+
